0f0a6678305f7490f178709cd6a34a31f5568c1e
VRUSHABH DEEPAK GAWDE <vrushabhgawde143@gmail.com>
2025-09-28 16:42:30 +0530

    changed URL to environmental variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9090/api", // adjust to your Spring Boot base path
+  baseURL: import.meta.env.VITE_API_BASE, // adjust to your Spring Boot base path
   headers: {
     "Content-Type": "application/json",
   },
